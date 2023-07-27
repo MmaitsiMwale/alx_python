66ee68109f4e77af2c8be669d827fd9444681caa
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-07-27 03:07:32 +0300

    matrix try 3

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 0:
-        print("")
+        print(" ")
     for i in matrix:
         for j, el in enumerate(i):
             if j == len(i) - 1:
