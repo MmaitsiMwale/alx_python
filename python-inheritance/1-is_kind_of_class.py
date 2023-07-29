b6cba5ea8e83f38d729029a3d1d29d6fe017c0e0
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-07-30 02:43:15 +0300

    issubclass

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,11 +1,10 @@
 """
 a function that returns True if the object is an instance of,
 or if the object is an instance of a class that inherited from,
-the specified class ; otherwise False. 
-#  and type(obj) == a_class
+the specified class ; otherwise False.
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """return True if object is instance or subclass of a class""" 
+    """return True if object is instance or subclass of a class"""
     return issubclass(type(obj), a_class)
