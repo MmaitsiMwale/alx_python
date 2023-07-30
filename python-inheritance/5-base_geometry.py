a232fe498d7c1333a57b05e9252398e0ec61ff68
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-07-31 00:11:05 +0300

    validate-value

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -15,7 +15,11 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates integer"""
-        if isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        try:
+            isinstance(value, int)
+        except:
+            raise TypeError(f"{name} must be an integer")
+        try:
+            value > 0
+        except:
+            raise ValueError(f"{name} must be greater than 0")
