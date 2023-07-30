d49b9ce605b25747b0dac54a4fb03e16b188075f
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-07-30 23:40:08 +0300

    geometry-class

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,15 +1,17 @@
 """
 Geometry Class based on 3-base_geometry with non defined
-method area()
+method area().
 """
 
 
 class BaseGeometry(BaseGeometry):
-    """Geometry Class"""
+    """
+    Geometry Class with undefined area() method
+    """
 
     def __init__(self):
         super().__init__()
 
     def area(self):
-        """raise exception"""
+        """raise exception that area is not defined"""
         raise Exception("area() is not implemented")
