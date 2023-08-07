23dab02080f74b2ea65a1e07bbaf45b2343bb621
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-07 13:23:53 +0300

    base-class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,4 +9,15 @@ class Base:
         if not id:
             self.id = id
         else:
-            self.__class__.__nb_objects += 1
+            new_id = __class__.__nb_objects + 1
+            self.id = new_id
+
+    # @id
+    # def id():
+    #     """get the id passed to the function"""
+    #     pass
+
+    # @property
+    # def id():
+    #     """set the id with condition"""
+    #     pass
