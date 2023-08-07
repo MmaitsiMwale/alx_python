228ec1595890c1db20b52d1cb7e25a2f75810b8f
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-07 13:32:02 +0300

    base-class

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,11 +6,11 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        if not id:
-            self.id = id
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
         else:
-            new_id = __class__.__nb_objects + 1
-            self.id = new_id
+            self.id = id
 
     # @id
     # def id():
