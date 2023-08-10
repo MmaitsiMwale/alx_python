a745de45f8d7e88385aed8b369facc995de56a88
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-10 04:02:45 +0300

    task-0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,10 @@
-import requests
-
+#!/usr/bin/python3
 """python scripts that fetches https://alu-intranet.hbtn.io/status"""
 
-page = https://alu-intranet.hbtn.io/status
-requests.get(page)
+
+import requests
+
+
+page = "https://alu-intranet.hbtn.io/status"
+req = requests.get(page)
+print(f"- type: {type(req.text)}\n- content: {req.status_code}")
---
+++ python-network_1/README.md
@@ -0,0 +1,5 @@
+# Python - Network
+
+This project covers Network in python.
+We will cover topics such as HTTP, URLs, requests among other.
+Under HTTP, we will write code that performs GET, POST, and other requests to servers.
