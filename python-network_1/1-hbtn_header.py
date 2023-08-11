f916216de90daf720a54a4ba53b5e31b77d5dbe8
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-11 03:27:30 +0300

    headers

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
-"""module to display the value of X-Request-Id in the 
-response header"""
+"""module to display the value of X-Request-Id in the response header"""
 
 
 import requests
 import sys
 
+"""collect the first argument and use it as a url"""
 page = sys.argv[1]
 req = requests.get(page)
 print(req.headers["X-Request-Id"])
