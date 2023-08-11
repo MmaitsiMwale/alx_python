a791fcc97db8f8ea40f14face694e3989b1f9b5e
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-11 04:29:54 +0300

    task-2

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+"""module that takes in a URL and an email address, sends a POST request
+    to the parsed URL with the email as the parameter and finally displays
+    the body of the response
+"""
+
+import requests
+import sys
+
+"""function that takes in url string and then uses it to send email"""
+try:
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data=email)
+    print(f"Your email is: {r.status_code}")
+except Exception as error:
+    print("Something is not working")
