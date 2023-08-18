e17e7df144d5bd58053a2990fdf3e2b1dea4e2af
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-18 18:41:12 +0300

    revised

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,15 +1,30 @@
+#!/usr/bin/python3
+
 """module that takes in a URL and an email address, sends a POST 
 request to the parsed URL with the email as the parameter"""
 
 import requests
 import sys
 
-"""function that takes in url string and then uses it to send email"""
-try:
-    url = sys.argv[1]
-    email = sys.argv[2]
-    r = requests.post(url, data=email)
-    if r.status_code:
-        print(f"Email: {email}")
-except requests.exceptions.HTTPError as error:
-    print("Something is not working")
+
+def post(url, email):
+    """sends a POST request to url with email as data"""
+    try:
+        r = requests.post(url, data=email)
+        if r.status_code:
+            return f"Email: {email}"
+    except requests.exceptions.HTTPError as error:
+        error = str(error).split(':')[-1].strip()
+        return f"Something is not working: {error}"
+
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        # check for valid input
+        assert len(sys.argv) == 3, "Invalid number of arguments."
+        success = post(url, email)
+        print(success)
+    except Exception:
+        print(f"Error--> Correct Usage: <./program.py> <url> <email>")
