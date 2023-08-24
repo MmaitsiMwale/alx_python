e0e5b859c2c504df24034c111fced91096db4c76
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-24 03:47:02 +0300

    readme-select-all

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""Script that lists all states that start with N from the database hbtn_0e_0_usa"""
+
+
+from MySQLdb import connect
+import sys
+
+
+def list_all_states(username, password, database_name):
+    """
+    lists all states in the table states from the database hbtn_0e_0_usa
+    """
+    # get connection details
+    conn = connect(
+        user=username,
+        passwd=password,
+        db=database_name,
+        host="localhost",
+        port=3306,
+    )
+
+    cursor = conn.cursor()
+
+    # Create a query to get all states from the table states:
+    select_query = "SELECT * FROM `states` WHERE name LIKE 'N%'"
+    cursor.execute(select_query)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    try:
+        assert len(sys.argv) == 4
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database_name = sys.argv[3]
+        list_all_states(username, password, database_name)
+    except AssertionError:
+        print(f"Usage : ./{__name__} username password databasename")
