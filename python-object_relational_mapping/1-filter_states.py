6e1ab6b2e02e757e4630067c9ba51527b8085757
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-24 04:01:13 +0300

    Capital-N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""Script that lists all states that start with N from the database hbtn_0e_0_usa"""
+"""Script that lists all states that start with N from the database 
+hbtn_0e_0_usa"""
 
 
 from MySQLdb import connect
