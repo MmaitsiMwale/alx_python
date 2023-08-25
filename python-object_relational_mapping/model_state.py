0af3911830711abeea69860acb100efaca1ad567
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-25 22:49:07 +0300

    class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,8 +1,9 @@
+"""python file that contains the class definition of a
+State and an instance Base = declarative_base()"""
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
-"""python file that contains the class definition of a
-State and an instance Base = declarative_base()"""
 
 Base = declarative_base()
 
