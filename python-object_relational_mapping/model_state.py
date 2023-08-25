39c46b408b2b3bd86338c008f8a110e4656892d5
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-25 04:57:37 +0300

    class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,17 +3,19 @@
 """python file that contains the class definition of a
 State and an instance Base = declarative_base()"""
 
-from sqlalchemy import create_engine, Column, Integer, DateTime, String
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 from config import DATABASE_CONFIG
-import datetime as dt
 
 
 Base = declarative_base()
 
 
 class State(Base):
-    """represents an instant of the table states"""
+    """represents an instant of the table states
+        id: primary key for states table
+        name : name of state
+    """
 
     __tablename__ = 'states'
     id = Column('id', Integer, primary_key=True)  # primary key
