400e3fa524826a8207408a683b05a0a44bafe96e
MmaitsiMwale <mmaitsimwale@gmail.com>
2023-08-29 16:44:03 +0300

    route-2

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,23 @@
+"""Simple script that starts a flask web application
+listens on 0.0.0.0, port 5000
+Routes:
+    /: display Hello HBNB!"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# decorator to register a route with the app, '/' is the root path of our web application and will be used
+@app.route("/")
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
